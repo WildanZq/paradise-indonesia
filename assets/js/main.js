7812c87ffc963145b12bbe971f7b3b4c698009b5
Wildan Ziaulhaq <wildan2wildan@gmail.com>
2018-03-27 17:46:32 +0700

    Fix gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,14 @@ $('#nav-button').click(() => {
 		$('#nav').addClass('active');
 	}
 });
+$('.love').click((e) => {
+	if ($(e.target).hasClass('active')) {
+		$(e.target).next()[0].innerHTML = parseInt($(e.target).next()[0].innerHTML) - 1;
+		$(e.target).attr('src','assets/icon/love-empty.png');
+		$(e.target).removeClass('active');
+	} else {
+		$(e.target).next()[0].innerHTML = parseInt($(e.target).next()[0].innerHTML) + 1;
+		$(e.target).attr('src','assets/icon/love-full.png');
+		$(e.target).addClass('active');
+	}
+});
